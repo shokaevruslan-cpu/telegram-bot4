b5ad0be74f87c8cb51374a07acc8f38c0abf13aa
shokaevruslan-cpu <shokaevruslan@gmail.com>
2025-09-20 23:57:27 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,13 @@
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
 
-# Токен бота в переменной окружения BOT_TOKEN
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Обработчик команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Я простой бот.")
+    await update.message.reply_text("Привет! Я бот.")
 
-# Точка входа
-if __name__ == "__main__":
-    # Создаём приложение бота
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    
-    # Добавляем команду /start
-    app.add_handler(CommandHandler("start", start))
-    
-    # Запускаем бота
-    print("Бот запущен...")
-    app.run_polling()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
